1d428671dbf05a7d2e1744a237f11de7376bff4b
Robert Koeninger <rkoeninger@att.net>
2017-04-26 11:24:37 -0400

    Generalized fetch.bat using variables

--- fetch.bat
+++ fetch.bat
@@ -1,5 +1,16 @@
+set Version=20.0
+
+set UrlRoot=https://github.com/Shen-Language/shen-sources/releases/download
+set ReleaseName=shen-%Version%
+set FileName=ShenOSKernel-%Version%.zip
+set NestedFolderName=ShenOSKernel-%Version%
+
 del .\kernel /q /s /f
-PowerShell.exe -Command "Invoke-WebRequest -Uri https://github.com/Shen-Language/shen-sources/releases/download/shen-20.0/ShenOSKernel-20.0.zip -OutFile .\ShenOSKernel-20.0.zip"
-PowerShell.exe -Command "Expand-Archive .\ShenOSKernel-20.0.zip -DestinationPath ."
-ren ShenOSKernel-20.0 kernel
-del ShenOSKernel-20.0.zip /q /f
+powershell.exe -Command "Invoke-WebRequest -Uri $Env:UrlRoot/$Env:ReleaseName/$Env:FileName -OutFile .\$Env:FileName" || goto fail
+powershell.exe -Command "Expand-Archive .\$Env:FileName -DestinationPath ." || goto fail
+ren %NestedFolderName% kernel
+del %FileName% /q /f
+exit /b
+
+fail:
+exit /b %ERRORLEVEL%
